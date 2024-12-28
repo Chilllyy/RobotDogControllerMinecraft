c786c42dae15a0c2003864cce0e145ca88abce2d
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-12-28 13:46:23 -0700

    added dependencies I will need

--- src/client/java/me/chillywilly/RobotDogControllerClient.java
+++ src/client/java/me/chillywilly/RobotDogControllerClient.java
@@ -1,10 +1,16 @@
 package me.chillywilly;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class RobotDogControllerClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		
+		ServerLivingEntityEvents.AFTER_DEATH.register((entity, source) -> {
+			if (entity instanceof PlayerEntity) {
+				RobotDogController.LOGGER.info("Player Death!");
+			}
+		});
 	}
 }
