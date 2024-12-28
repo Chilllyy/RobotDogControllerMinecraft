c0dfb93ccf31c8a7af664ef7c78917debae13d8c
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-12-28 12:56:38 -0700

    init

---
+++ src/client/java/me/chillywilly/RobotDogControllerClient.java
@@ -0,0 +1,10 @@
+package me.chillywilly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RobotDogControllerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
